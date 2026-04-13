4a1604dbaa4f3dfff2a7be7eb2ff6479c52a5fb3
Mikxus <91991837+Mikxus@users.noreply.github.com>
2026-04-13 21:01:53 +0300

    feat: add new pages

---
+++ desktop-app/src/main/java/com/project/app/views/DashboardView.java
@@ -0,0 +1,29 @@
+package com.project.app.views;
+
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
+import com.project.app.views.AbstractView;
+import com.project.app.navigation.AppNavigator;
+
+public class DashboardView extends AbstractView {
+    
+    public DashboardView(AppNavigator navigator) {
+        super(navigator, 300, 600);
+    }
+
+    @Override
+    protected final Parent build() {
+        Button btn = new Button("Dashboard");
+        btn.setOnAction(event -> {
+            System.out.println("calculating stuff");
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        return root;
+    }
+}
+
---
+++ desktop-app/src/main/java/com/project/app/views/LoginView.java
@@ -0,0 +1,46 @@
+package com.project.app.views;
+
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+
+
+// Icons
+import org.kordamp.ikonli.javafx.FontIcon;
+import org.kordamp.ikonli.material2.Material2MZ;
+
+import com.project.app.navigation.AppNavigator;
+import com.project.app.component.UrlTextField;;
+
+public class LoginView extends AbstractView {
+    
+    public LoginView(AppNavigator navigator) {
+        super(navigator, 400, 600);
+    }
+
+    @Override
+    protected final Parent build() {
+        var icon = new FontIcon(Material2MZ.VPN_KEY);
+
+        var vbox_layout = new VBox(20);
+        var server_url = new UrlTextField("Server address", "https://test.mikxus.dev");
+
+        icon.setStyle("-fx-icon-size: 96;");
+
+        // Layout
+        vbox_layout.setPadding(new Insets(30, 20, 0, 20));
+        vbox_layout.setAlignment(javafx.geometry.Pos.TOP_CENTER);
+
+        Button btn = new Button("Login with Authentik");
+        btn.setOnAction(event -> {
+            System.out.println("Opening browser for Authentik...");
+            navigator.showDashboard();
+        });
+
+        vbox_layout.getChildren().addAll(icon, server_url);
+        return vbox_layout;
+    }
+}
+
