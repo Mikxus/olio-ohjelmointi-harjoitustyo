63e83b3e5560ae8f081d4aba2465d68b037292e0
Mikxus <91991837+Mikxus@users.noreply.github.com>
2026-04-19 16:16:43 +0300

    fix: use common api contract

--- backend/src/main/java/com/project/backend/StatusResource.java
+++ backend/src/main/java/com/project/backend/StatusResource.java
@@ -1,15 +1,19 @@
 package com.project.backend;
 
+import com.project.common.api.StatusApi;
+import com.project.common.api.dto.StatusResponse;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/status")
-public class Status {
+public class StatusResource implements StatusApi {
+    @Override
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String status() {
-        return "1";
+    public StatusResponse getStatus() {
+        return true;
     }
 }
