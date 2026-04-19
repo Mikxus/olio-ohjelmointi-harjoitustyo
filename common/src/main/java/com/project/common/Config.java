07ce6141a91fede46400f6115a133a965249b68f
Mikxus <91991837+Mikxus@users.noreply.github.com>
2026-04-20 00:58:25 +0300

    feat: add property saving to file

--- common/src/main/java/com/project/common/Config.java
+++ common/src/main/java/com/project/common/Config.java
@@ -1,13 +1,20 @@
 package com.project.common;
 
 import java.util.NoSuchElementException;
+import java.util.Properties;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.nio.file.Path;
 
 /**
  * Config helpers
  */
 public final class Config {
-    private Config() {}
+    private static Properties props = new Properties();
+
+    private Config() {
+        loadProps();
+    }
 
     private static void checkAndThrowNotFound(String value, String envName) {
         if (value == null || value.isBlank() == true) {
@@ -15,9 +22,55 @@ public final class Config {
         }
     }
 
+    private static String getPropsPath() {
+        return getPropertiesStoreDir() + "app.properties"; 
+    }
+
+    private static void saveProps() {
+        try {
+            FileWriter writer = new FileWriter(getPropsPath());
+            props.store(writer, getOauthAuthorizationUrl());
+            writer.close();
+        } catch (Exception e) {
+            System.err.println("Failed to store properties: " + e.toString());
+        }
+    }
+
+    private static void loadProps() {
+        try {
+            FileReader reader = new FileReader(getPropsPath());
+            props.load(reader);
+            reader.close();
+        } catch (Exception e) {
+            System.err.println("Failed to read properties: " + e.toString());
+        }
+    } 
+
     public static boolean isProd() {return "prod".equalsIgnoreCase(System.getenv("ENV"));}
     public static boolean isDev() {return "dev".equalsIgnoreCase(System.getenv("ENV"));}
     public static boolean allowHTTP() {return Boolean.parseBoolean(System.getenv("ALLOW_HTTP"));}
+
+    public static boolean isBackendUrl() {
+        String url = getBackendUrl();
+        if (url == null || url.isBlank() == true)
+            return false;
+
+        return true;
+    }
+
+    public static String getBackendUrl() {
+        loadProps();
+        return props.getProperty("backendUrl");
+    }
+
+    public static void setBackendUrl(String url) {
+        props.setProperty("backendUrl", url);
+        saveProps();
+    }
+
+    public static String getPropertiesStoreDir() {
+        return System.getProperty("APP_PROPERTIES_STORE_DIR", "~/.project-app/");
+    }
 
     public static String getOauthClientID() throws NoSuchElementException {
         String clientID = System.getenv("APP_OAUTH_CLIENT_ID");
